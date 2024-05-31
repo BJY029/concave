5acfa90a759da50e664ebc3c49250146b8a795b4
sjyb29 <sjyb29@sju.ac.kr>
2024-05-31 09:32:02 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "head.h"
 
 int main() {
+	system("mode con:cols=150 lines=150");
 	checkerBoard start; //게임시작(생성자로 인해 콘솔에 게임판과 인터페이스 그리드 출력)
 	start.updateCheckerBoard();//착수 과정 시작
 	//Sleep(3000);
